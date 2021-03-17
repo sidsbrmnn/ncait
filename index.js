c7336073adbe829a63fde9cb02d02a704b3b1d7e
Siddharth Subramanian <sidsbrmnn@gmail.com>
2021-03-17 15:58:53 +0530

    Update views

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const expbhs = require('express-handlebars');
 const nodemailer = require('nodemailer');
+const path = require('path');
+
+const paperTopics = [
+    'Machine Learning',
+    'AI & Robotics',
+    'Data Mining',
+    'Big Data Analytics',
+    'Cloud Computing',
+    'Networking',
+    'Internet of Things',
+    'Image Processing',
+    'Parallel Computing',
+    'Web Services',
+    'Wireless Sensor Networks',
+    'Information & Data Security',
+    'Grid Computing',
+    'Speech Processing',
+    'Multimedia Communication',
+    'Natural Language Processing',
+    'Pattern Recognition',
+    'Bioinformatics',
+    'Ad-Hoc Networks',
+    'Neural Networks',
+    'Computer Graphics',
+    'Visualisation',
+];
 
 const app = express();
 
@@ -12,32 +38,24 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-app.engine(
-    'hbs',
-    expbhs({
-        extname: 'hbs',
-        defaultLayout: 'default',
-        layoutsDir: __dirname + '/views/layouts/',
-        partialsDir: __dirname + '/views/partials/',
-    })
-);
-app.set('view engine', 'hbs');
+app.engine('handlebars', expbhs());
+app.set('view engine', 'handlebars');
 
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
 app.get('/guidelines', (req, res) => {
-    res.render('guidelines');
+    res.render('guidelines', { topics: paperTopics });
 });
 
-app.post('/submit', (req, res) => {
-    transporter
-        .sendMail({
+app.post('/submit', async (req, res) => {
+    try {
+        await transporter.sendMail({
             from: '"Contact Form" <sidsbrmnn@gmail.com>',
             to: 'jssncait2019@gmail.com',
             subject: 'Message from contact form',
@@ -46,17 +64,20 @@ app.post('/submit', (req, res) => {
 From
 ${req.body.name}
 ${req.body.email}`,
-        })
-        .then((success) => res.send('Mail sent.'))
-        .catch((error) => {
-            console.log(error);
-            res.send('Mail cannot be sent at the moment.');
         });
+
+        res.send('Mail sent.');
+    } catch (err) {
+        console.error(err);
+        res.send('Mail cannot be sent at the moment.');
+    }
 });
 
 app.get('*', (req, res) => {
-    res.render('404', { template: 'body-404' });
+    res.render('404', { layout: 'error' });
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+app.listen(PORT, () => {
+    console.log('Listening on port :' + PORT);
+});
